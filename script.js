60e3cde78f7c439ef5bb387fd7dcf049177a6496
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-06-07 21:46:48 +0800

    code

---
+++ script.js
@@ -0,0 +1,89 @@
+// var arr =[2,4,"hey",true,6.5,"hello",false,8];
+// arr.forEach(function(val) {
+//     console.log(val + " hello");
+// })
+
+// var arr=[1,2,3,4];
+
+// var ans= arr.map(function(val) {
+//     return val * 2;   
+// })
+// console.log(ans);
+
+// var arr=[1,2,3,4];
+// var ans=arr.filter(function(val) {
+//     if(val >= 3) { return true; }
+//     else { return false; }
+// })
+// console.log(ans);
+
+// var arr= [1,2,3,4];
+// var ans=arr.find(function(val) {
+//     if(val === 3) return true; 
+// })
+// console.log(ans);
+
+//object
+
+// var obj = {
+//    name: "John",
+//    age: 30,
+
+// }
+// obj.name
+
+// function val() {
+//    return 3;
+
+// }
+// var ans=val();
+
+
+
+//async js
+
+
+   //node.js
+
+   
+
+const fs= require('fs');
+
+// fs.appendFile('hey.txt', 'accha huu', function(err) {
+//       if(err) console.log(err);
+//       else console.log("file written successfully");
+// })
+
+// fs.rename("hey.txt", "hello.txt", function(err) {
+//       if(err) console.log(err);
+//       else console.log("done");
+// })
+
+// fs.copyFile("hello.txt", "./copy/copy.txt", function(err) {
+//       if(err) console.log(err);
+//       else console.log("file copied successfully");
+// })
+
+// fs.unlink("hello.txt", function(err) {
+//       if(err) console.error(err);
+//       else console.log("file deleted successfully");
+// })
+
+// fs.rmdir("./copy", {recursive: true }, function(err) {
+//       if(err) console.error(err);
+//       else console.log("directory deleted successfully");
+// })
+
+
+
+//HTTP module
+
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+      res.end("hello world");
+})
+
+server.listen(3000, function() {
+      console.log("server is running on port 3000");
+})
